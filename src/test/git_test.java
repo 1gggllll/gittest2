ba16ea8aa1a36495eb4f86b8340afe8a4fd49a9f
1ggglll <2767882448@qq.com>
2024-03-26 19:42:16 +0800

    my first commit

---
+++ src/test/git_test.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * ClassName: git_test
+ * Package: test
+ * Description:
+ *
+ * @Author LLLLB
+ * @Create 2024/3/26 19:34
+ * @Version 1.0
+ */
+public class git_test {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+    }
+}
